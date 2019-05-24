101aad447ae638e9da097052c0752a5c560ad4ae
jesse collier <jesse.collier97@gmail.com>
2019-05-24 20:51:39 +1200

    changing git ignore

---
+++ client/components/Map.jsx
@@ -0,0 +1,17 @@
+import React, { Component, Fragment as F } from 'react';
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <F>
+        
+      </F>
+     );
+  }
+}
+ 
+export default Map;
