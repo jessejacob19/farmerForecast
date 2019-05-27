c54ef8a78721bcbed2c6e22b90c2af336a7c3f5e
jesse collier <jesse.collier97@gmail.com>
2019-05-27 17:24:41 +1200

    fixed webpack issues

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,34 +1,41 @@
 import React, { Component, Fragment as F } from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 
-import { fetchEmojiAction } from "../actions/emoji";
+// import { fetchEmojiAction } from "../actions/emoji";
+// import SimpleMap from "./SimpleMap";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  componentDidMount() {
-    this.props.fetchEmoji();
-  }
+  // componentDidMount() {
+  //   this.props.fetchEmoji();
+  // }
   render() {
     return (
       <F>
         <p>react is running</p>
+        {/* <SimpleMap /> */}
       </F>
     );
   }
 }
-function mapStateToProps(state) {
-  return state;
-}
+// function mapStateToProps(state) {
+//   return state;
+// }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchEmoji: () => {
-      dispatch(fetchEmojiAction())
-    }
-  }
-}
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     fetchEmoji: () => {
+//       dispatch(fetchEmojiAction());
+//     }
+//   };
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
+
+export default App;
