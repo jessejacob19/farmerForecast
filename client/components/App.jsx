808938d08c97eb7cf40551239dcb7ae8ec105ce6
jesse collier <jesse.collier97@gmail.com>
2019-05-28 13:25:03 +1200

    got the google map rendering something

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,41 +1,29 @@
 import React, { Component, Fragment as F } from "react";
-// import { connect } from "react-redux";
-
-// import { fetchEmojiAction } from "../actions/emoji";
-// import SimpleMap from "./SimpleMap";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  // componentDidMount() {
-  //   this.props.fetchEmoji();
-  // }
   render() {
+    console.log(this.props.google);
     return (
       <F>
-        <p>react is running</p>
-        {/* <SimpleMap /> */}
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            {/* <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div> */}
+          </InfoWindow>
+        </Map>
       </F>
     );
   }
 }
-// function mapStateToProps(state) {
-//   return state;
-// }
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     fetchEmoji: () => {
-//       dispatch(fetchEmojiAction());
-//     }
-//   };
-// }
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(App);
-
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCDY6eWTVLvpOoTI2JrH8Q0ycDSV3F2J5o"
+})(App);
